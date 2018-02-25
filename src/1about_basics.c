37cdec9aa687608f7783cc6daccb1806dceec86c
Fanng Dai <fdai@cs.stonybrook.edu>
2018-02-25 00:30:13 -0500

    about_basics

--- src/1about_basics.c
+++ src/1about_basics.c
@@ -2,6 +2,10 @@
 #include <limits.h>
 #include <stdbool.h>
 
+/**
+ * You need to change the values s.t. they match
+ * What is false and what is true?
+ */
 Test(about_basics, intro)
 {
     /*
@@ -14,7 +18,7 @@ Test(about_basics, intro)
         Tests are built on assertions, simple evaluations of truth in order to
         assess the correctness of code
     */
-    cr_assert(false,
+    cr_assert(true,
         "This should be true, assertion - a confident and forceful "
         "statement of fact");
     /*
@@ -26,43 +30,47 @@ Test(about_basics, intro)
         interpret
         the input as an int
     */
-    cr_assert(false == 1,
+    cr_assert(false == 0,
         "In C there is no false, there is only zero. false is in fact: %d",
         false);
-    cr_assert(true == 0,
+    cr_assert(true == 1,
         "In C there is no true, there is only not zero. true is in fact: %d",
         true);
     /*
         Criterion has more descriptive tests that can evaluate the same thing
         eq is short for equals
     */
-    cr_assert_eq(false, 1, "Nothing is not something");
-    cr_assert_eq(true, 0, "Something is not nothing");
+    cr_assert_eq(false, 0, "Nothing is not something");
+    cr_assert_eq(true, 1, "Something is not nothing");
 }
 
+/**
+* Replace TODO to the actual value in decimal?
+*/
 Test(about_basics, variables)
 {
     /*
         If you've had some experience with Java then C won't seem too foreign in
         terms of syntax
     */
-    char c = TODO;
+    char c = 67;
     cr_assert_eq(c, 'C',
         "All characters in C are interpreted from the ASCII "
         "table, go to your terminal and enter 'man ascii' to "
         "find this solution");
 
-    short s = TODO;
+    short s = 3054;
     cr_assert_eq(s, 0xBEE, "A short is 2 bytes");
 
-    int i = TODO;
+    int i = 195935983;
     cr_assert_eq(i, 0xBADBEEF, "A int is 4 bytes");
 
-    long l = TODO;
+    long l = 59788208926;
     cr_assert_eq(l, 0xDEBA7AB1E,
         "A long is a larger integer type than int (supports unsigned).");
 
-    unsigned int ui = TODO;
+    // I'm not sure about this... bc it's gt? so greater than...
+    unsigned int ui = 3735928560;
     /*
         This unsigned int should exceed the maximum capacity of an INT (hint
         INT_MAX is declared in limits.h)
@@ -72,20 +80,32 @@ Test(about_basics, variables)
         "primitive data type to increase the upper "
         "limit by only storing positive values");
 
-    long long ll = TODO;
+    //
+    ui = 3735928559;
+    /*
+        This unsigned int should exceed the maximum capacity of an INT (hint
+        INT_MAX is declared in limits.h)
+    */
+    cr_assert_eq(ui, 0xDEADBEEF,
+        "The unsigned modifier can be used on a "
+        "primitive data type to increase the upper "
+        "limit by only storing positive values");
 
+    long long ll = 255;
+
+    // You will also have to change the value to some of these so that they match
     cr_expect_eq(ll, 0xFF,
         "A number literal starting with 0x will be interpreted as hexadecimal");
     cr_expect_eq(ll, 0b11111111,
         "A number literal starting with 0b will be interpreted as binary");
-    cr_assert_eq(ll, 0777,
+    cr_assert_eq(ll, 0377,
         "A number literal starting with 0 will be interpreted as octal");
 
-    double d = 3.50;
+    double d = 7/2;
     cr_assert_float_eq(d, (7 / 2), 0.000001,
         "Just like Java, C does integer division for 7/2");
 
-    double d2 = 350e-2;
+    double d2 = 3 + (1 / 2);
     cr_assert_float_eq(d2, 3 + (1 / 2), 0.000001,
         "Addition also effects whether a number literal is "
         "interpreted as IEEE or 2's Comp");
